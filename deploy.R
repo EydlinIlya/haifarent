e5711e0b676f31821b7a64622d7353b393a06387
Ilya Eydlin <ilyabeydlin@gmail.com>
2024-08-16 10:33:21 +0300

    Minimal working example

---
+++ deploy.R
@@ -0,0 +1,5 @@
+library(rsconnect)    # Free deployment option
+
+setAccountInfo(name=Sys.getenv("POSIT_NAME"), token=Sys.getenv("POSIT_TOKEN"),
+               secret=Sys.getenv("POSIT_SECRET"))
+deployApp()
