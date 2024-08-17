1373951e725ed8353c5186daacc31902cfd91c6d
Ilya Eydlin <ilyabeydlin@gmail.com>
2024-08-17 18:10:27 +0300

    deploy.R  now deploys only UI and geo.csv. Minor changes in the parser.Rmd

--- deploy.R
+++ deploy.R
@@ -2,4 +2,4 @@ library(rsconnect)    # Free deployment option
 
 setAccountInfo(name=Sys.getenv("POSIT_NAME"), token=Sys.getenv("POSIT_TOKEN"),
                secret=Sys.getenv("POSIT_SECRET"))
-deployApp()
+deployApp(appFiles = c("faceshtime.Rmd", "geo.csv"))
